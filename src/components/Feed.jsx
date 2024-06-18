e84605c3a3bf9ec910bbd76fd8cf62079a87dd4a
Arjun <arjunsuthar2112@gmail.com>
2024-06-18 17:29:16 +0530

    bug resolved

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -10,7 +10,7 @@ const Feed = () => {
 
   return (
 
-    <div className='flex flex-row h-[calc(100%-56px)]'>
+    <div className='flex flex-row h-[calc(100%)]'>
       <LeftNav />
       <div className="sm:w-[100vw]  h-full overflow-y-auto scrollbar-hide bg-black">
         <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-10 py-5 '>
